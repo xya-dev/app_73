a984e3e8e0f525503c8ea01344a8282e7f92c0b8
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-07-15 11:40:59 +0200

    feat: Google and Github login (#16)

---
+++ lib/app_73_web/controllers/auth_controller.ex
@@ -0,0 +1,38 @@
+defmodule App73Web.AuthController do
+  @moduledoc """
+  Handles authentication callbacks and session management.
+  """
+
+  require Logger
+  use App73Web, :controller
+  plug Ueberauth
+
+  def callback(%{assigns: %{ueberauth_failure: _fails}} = conn, _params) do
+    conn
+    |> put_flash(:error, "Failed to authenticate.")
+    |> redirect(to: "/")
+  end
+
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
+    id = auth.uid
+    # email = auth.info.email
+    provider = Atom.to_string(auth.provider)
+
+    success(conn, "#{provider}-#{id}")
+  end
+
+  def delete(conn, _params) do
+    conn
+    |> delete_session(:user_id)
+    |> put_flash(:info, "Logged out.")
+    |> redirect(to: ~p"/")
+  end
+
+  defp success(conn, user_id) do
+    conn
+    |> put_flash(:info, "Successfully authenticated.")
+    |> put_session(:user_id, user_id)
+    |> configure_session(renew: true)
+    |> redirect(to: ~p"/")
+  end
+end
