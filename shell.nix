68f6ed2bf30ea8a421f7dc14f0f3816929041507
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-08-16 22:56:07 +0200

    chore: Add ngrok to nix shell (#29)

--- shell.nix
+++ shell.nix
@@ -1,9 +1,12 @@
 let
-  pkgs = import <nixpkgs> { };
+  pkgs = import <nixpkgs> {
+    config.allowUnfree = true;
+  };
 in
 pkgs.mkShell {
   packages = with pkgs; [
     beam.packages.erlang_27.elixir_1_18
     podman-compose
+    ngrok
   ];
 }
