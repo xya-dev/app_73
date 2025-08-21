1bf04da4b006cfcc64e487d0a445153a28635aa9
Mateusz Ledwoń <mateuszledwon@duck.com>
2025-08-21 21:47:52 +0200

    chore: Move common actor part to macro

--- lib/app_73/domain/profile/actor.ex
+++ lib/app_73/domain/profile/actor.ex
@@ -3,15 +3,14 @@ defmodule App73.Domain.Profile.Actor do
   Represents user profile.
   """
 
-  require Logger
-
   alias App73.Database.ProfileRepository
   alias App73.Domain.Profile.Command
 
-  use GenServer, restart: :transient
-
-  # 15 minutes in milliseconds
-  @timeout 15 * 60 * 1000
+  use App73.Utils.Actor,
+    registry: App73.Domain.Profile.Registry,
+    supervisor: App73.Domain.Profile.Supervisor,
+    # 15 minutes in milliseconds
+    timeout: 15 * 60 * 1000
 
   @type t :: %__MODULE__{
           id: String.t(),
@@ -29,39 +28,13 @@ defmodule App73.Domain.Profile.Actor do
     :created_at
   ]
 
-  def get(profile_id) when is_binary(profile_id) do
-    case Horde.DynamicSupervisor.start_child(
-           App73.Domain.Profile.Supervisor,
-           {__MODULE__, profile_id}
-         ) do
-      {:ok, pid} ->
-        Logger.debug("Started profile actor with ID: #{profile_id}")
-        {:ok, pid}
-
-      {:error, {:already_started, pid}} ->
-        Logger.debug("Profile actor with ID: #{profile_id} already started")
-        {:ok, pid}
-
-      {:error, reason} ->
-        Logger.error(
-          "Failed to start profile actor with ID: #{profile_id}, reason: #{inspect(reason)}"
-        )
-
-        {:error, reason}
-    end
-  end
-
   def generate_id(provider, provider_id) when is_binary(provider) and is_binary(provider_id) do
     "#{provider}-#{provider_id}"
   end
 
   def init(id) do
     GenServer.cast(self(), {:load, id})
-    {:ok, %__MODULE__{}, @timeout}
-  end
-
-  def start_link(id) do
-    GenServer.start_link(__MODULE__, id, name: via_tuple(id))
+    {:ok, %__MODULE__{}}
   end
 
   def create(pid, cmd) do
@@ -82,40 +55,34 @@ defmodule App73.Domain.Profile.Actor do
     }
 
     case ProfileRepository.persist_actor(new_profile) do
-      {:ok, _} ->
-        {:reply, {:ok, new_profile}, new_profile, @timeout}
-
-      {:error, reason} ->
-        {:reply, {:error, reason}, state, @timeout}
+      {:ok, _} -> reply({:ok, new_profile}, new_profile)
+      {:error, reason} -> reply({:error, reason}, state)
     end
   end
 
   def handle_call(:state, _from, state) do
     case state.created_at do
-      nil -> {:reply, nil, state, @timeout}
-      _ -> {:reply, state, state, @timeout}
+      nil -> nil
+      _ -> state
     end
+    |> reply(state)
   end
 
   def handle_cast({:load, id}, _state) when is_binary(id) do
     case ProfileRepository.get_by_id(id) do
       nil ->
-        {:noreply,
-         %__MODULE__{
-           id: id
-         }, @timeout}
+        %__MODULE__{
+          id: id
+        }
 
       profile ->
-        {:noreply, profile, @timeout}
+        profile
     end
+    |> noreply()
   end
 
   def handle_info(:timeout, state) do
     Logger.debug("Profile actor with ID: #{state.id} timed out")
     {:stop, :normal, state}
-  end
-
-  defp via_tuple(profile_id) do
-    {:via, Horde.Registry, {App73.Domain.Profile.Registry, profile_id}}
   end
 end
